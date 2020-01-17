0e3fc1820758bc2d0e9a137ca3c9e2934d79d130
lista <lista@bk.ru>
2020-01-17 14:15:15 +0200

    start

--- src/main/java/com/example/asyncmethod/AsyncMethodApplication.java
+++ src/main/java/com/example/asyncmethod/AsyncMethodApplication.java
@@ -11,10 +11,9 @@ import java.util.concurrent.Executor;
 /**
  * https://spring-projects.ru/guides/async-method/
  * https://spring.io/guides/gs/async-method/
- * https://translate.googleusercontent.com/translate_c?depth=1&rurl=translate.google.com.ua&sl=en&sp=nmt4&tl=ru&u=https://spring.io/guides/gs/async-method/&xid=17259,15700022,15700186,15700191,15700256,15700259,15700262,15700265,15700271&usg=ALkJrhgLjnXfq87hGqdo2wWl3JThwEI1Sw
-
   аннотация Spring @Async работает с веб-приложениями, но вам не нужно настраивать веб-контейнер, чтобы увидеть его преимущества.
  */
+// * https://translate.googleusercontent.com/translate_c?depth=1&rurl=translate.google.com.ua&sl=en&sp=nmt4&tl=ru&u=https://spring.io/guides/gs/async-method/&xid=17259,15700022,15700186,15700191,15700256,15700259,15700262,15700265,15700271&usg=ALkJrhgLjnXfq87hGqdo2wWl3JThwEI1Sw
 
 @SpringBootApplication
 @EnableAsync
